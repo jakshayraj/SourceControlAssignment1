17d3247f8a32139a87b99843e10f0102880b9810
akshyraj <akshayrajsinh324@gmail.com>
2020-11-27 16:03:07 +0530

    Add project files.

---
+++ SourceControlAssignment1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SourceControlAssignment1.Models;
+
+namespace SourceControlAssignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ SourceControlAssignment1/Models/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using SourceControlAssignment1.CustomValidation;
+using System.ComponentModel;
+
+namespace SourceControlAssignment1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is Required")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is Required")]
+        [MaxLength(10)]
+        [MinLength(3)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Age is Required")]
+        [Range(1, 80, ErrorMessage = "Age must be between 1-80 in years.")]
+        public int Age { get; set; }
+        [Required(ErrorMessage = "Email Id is Required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Emailid { get; set; }
+        [Required(ErrorMessage = "Email Id is Required")]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Incorrect Email Format")]
+        [DisplayName("Second Emaild")]
+        public string Second_Email { get; set; }
+        [Required(ErrorMessage = "Please enter hire date")]
+        [Display(Name = "Hire Date")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        [CustomHireDate(ErrorMessage = "Hire Date must be less than or equal to Today's Date")]
+        public DateTime HireDate { get; set; }
+        [Required(ErrorMessage = "Credit card is Required")]
+        [CreditCard(ErrorMessage = "Invalid credit card no.")]
+        [DisplayName("Credit Card")]
+        public int Credit_card { get; set; }
+        [Required(ErrorMessage = "Phone no is Required")]
+        [Phone(ErrorMessage = "Invalid Phone no")]
+        public int Phone { get; set; }
+    }
+}
